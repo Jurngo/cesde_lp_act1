82439ec29c7420a4505ed6c0fc8866b582144b24
Jurngo <juarango@cesde.net>
2026-05-16 16:31:49 +0000

    Avance

--- src/main/java/com/example/VariablesYConstantes.java
+++ src/main/java/com/example/VariablesYConstantes.java
@@ -3,10 +3,16 @@ package com.example;
 public class VariablesYConstantes {
     public static void demostrarDeclaracionAsignacion() {        
         // TODO
+        int edad =20;
+        double estatura = 1.8;
+        System.out.println(edad);
+        System.out.println(estatura);
     }
 
     public static void demostrarConvencionesNombres() {
         // TODO
+        string nombreJugador = "Juan" ;
+        int edadjugador =19;
     }
 
     public static void demostrarConstantes() {
